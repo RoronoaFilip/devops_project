8f2051cd48772b03c726b8ebe9cccf4968b7796d
Filip Filchev <fkfilchev@gmail.com>
2024-12-14 14:21:25 +0200

    Add Project files

---
+++ src/app.py
@@ -0,0 +1,17 @@
+import socket
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    hostname = socket.gethostname()
+    return "I am a Flask application running on {}".format(hostname)
+
+
+if __name__ == '__main__':
+    # Note the extra host argument. If we didn't have it, our Flask app
+    # would only respond to requests from inside our container
+    app.run(host='0.0.0.0')
---
+++ src/app_test.py
@@ -0,0 +1,17 @@
+import unittest
+
+from src.app import app
+
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+        self.client.testing = True
+
+    def test_hello_world(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
