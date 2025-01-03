68bc0a69d5fa50b4d256583900ee62df6203fff6
Filip Filchev <fkfilchev@gmail.com>
2025-01-03 18:40:49 +0200

    Change Docker port

--- install_and_run_docker_image.sh
+++ install_and_run_docker_image.sh
@@ -8,4 +8,4 @@ sudo yum install docker -y
 sudo service docker start
 sudo usermod -aG docker ec2-user
 
-sudo docker run -d -p 443:5000 $DOCKERHUB_TAG
+sudo docker run -d -p 5000:5000 $DOCKERHUB_TAG
